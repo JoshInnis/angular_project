f3e1920c7359e43b3992cc7283af2791d572e11c
CaptainNibblesworth <Josh.Innis@decentralclub.com>
2021-11-21 19:04:14 -0800

    ngFor
    Course: Angular Fundamentals
    Section: Rendering Flows
    Lesson: ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  name: string = "";
-
-  handleClick(value: string) {
-    console.log(value)
-    this.name = value;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }]
 }
