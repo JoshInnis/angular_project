b7f73dd990d254b3e071fad4b9bfe9becba0ac1f
CaptainNibblesworth <Josh.Innis@decentralclub.com>
2021-11-21 17:49:58 -0800

    Built First Component
    Course: Angular Fundamentals
    Section: Getting Started
    Lesson: First Component with @Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['app.component.scss'],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'angularProject';
+  title: string;
+
+  constructor() {
+    this.title = 'Ultimate Angular'
+  }
+
 }
