b8ee9636fdd1f948d1542297a93db1f9d35d2a71
CaptainNibblesworth <Josh.Innis@decentralclub.com>
2021-11-22 19:03:43 -0800

    Data Service and Dependency Injection
    Course: Angular Fundamentals
    Section: Services, Http, and Observables
    Lesson: Data Service and Dependency Injection

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 import { Passenger } from "../../models/passenger.interface";
 
+import { PassengerDashboardService } from "../../passenger-dashboard.service";
+
 @Component({
   selector: 'passenger-dashboard',
   styleUrls: ['passenger-dashboard.component.scss'],
@@ -10,33 +12,10 @@ import { Passenger } from "../../models/passenger.interface";
 export class PassengerDashboardComponent {
   passengers: Passenger[] = [];
 
+  constructor(private passengerService: PassengerDashboardService) {}
+
   ngOnInit() {
-    this.passengers = [{
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1491606000000,
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1488412800000,
-      children: [{ name: 'Jessica', age: 1 }]
-    }, {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-    }];
+    this.passengers = this.passengerService.getPassengers();
   }
 
   handleEdit(event: Passenger) {
---
+++ src/app/passenger-dashboard/passenger-dashboard.service.ts
@@ -0,0 +1,37 @@
+
+import { Passenger } from "./models/passenger.interface";
+
+export class PassengerDashboardService {
+
+
+    constructor() {}
+
+    getPassengers(): Passenger[] {
+        return  [{
+            id: 1,
+            fullname: 'Stephen',
+            checkedIn: true,
+            checkInDate: 1490742000000,
+          }, {
+            id: 2,
+            fullname: 'Rose',
+            checkedIn: false,
+            children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
+          }, {
+            id: 3,
+            fullname: 'James',
+            checkedIn: true,
+            checkInDate: 1491606000000,
+          }, {
+            id: 4,
+            fullname: 'Louise',
+            checkedIn: true,
+            checkInDate: 1488412800000,
+            children: [{ name: 'Jessica', age: 1 }]
+          }, {
+            id: 5,
+            fullname: 'Tina',
+            checkedIn: false,
+          }];
+    }
+}
